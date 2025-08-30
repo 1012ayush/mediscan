00278ada7bf4e0b04f86563f11b924d49715708c
1012ayush <ayush.24bce10624@vitbhopal.ac.in>
2025-08-30 21:23:40 +0530

    vercel1-commit

--- server/index.ts
+++ server/index.ts
@@ -47,25 +47,17 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
+
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
     serveStatic(app);
   }
+  if (process.env.VERCEL !== "1"){
+    const port = parseInt(process.env.PORT || "5000" , 10);
+    app.listen(port , () => {
+      log(`Server running locally at http://localhost:${port}`);} );
+    }
+  })();
 
-  // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-
-  server.listen({
-    port,
-    host: "localhost",
-  }, () => {
-    log(`serving on http://localhost:${port}`);
-  });
-})();
+export default app;
